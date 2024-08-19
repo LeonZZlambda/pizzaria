455315b8d34c2d9a4ca7aadb9240bac0f0511164
Leonzz_Lambda <leonzzlopes@proton.me>
2024-08-19 20:12:39 -0300

    pizza

--- pizzariaitb/src/App.jsx
+++ pizzariaitb/src/App.jsx
@@ -1,7 +1,12 @@
+import {useState} from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem';
 
-const App () => {
+const App = () => {
+
+  const [color] = useState("#00F")
+  document.body.style.backgroudColor = color 
 
   return (
     <Routes>
